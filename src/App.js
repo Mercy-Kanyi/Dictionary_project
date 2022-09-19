be6bc20f57de167ec662bb35dd6e10aad6bf083c
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-09-19 14:13:54 +0300

    added rel nonreferrer

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          Dictionary
-        </header>
+        <header className="App-header">Dictionary</header>
         <Dictionary />
         <footer className="App-footer">
           <small>
             Coded by{" "}
-            <a href="https://github.com/Mercy-Kanyi/Dictionary_project" target="_blank">
+            <a
+              href="https://github.com/Mercy-Kanyi/Dictionary_project"
+              target="_blank"
+              rel="noreferrer"
+            >
               Mercy Kanyi
             </a>
           </small>
