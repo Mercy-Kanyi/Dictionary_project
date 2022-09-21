d089d6a1ea0173a166a49e190f9b5ffbf3918f44
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-09-21 14:29:15 +0300

    changed the audio player

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,11 +1,10 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
 	return (
     <div className="Phonetics">
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a> <br />
+      <ReactAudioPlayer src={props.phonetics.audio} controls /> <br />
       {props.phonetics.text}
     </div>
   );
