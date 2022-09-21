5cea4fa998b4a4e744b6ca2a89be988633fd1101
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-09-21 14:12:35 +0300

    added phonetics

--- src/Definitions.js
+++ src/Definitions.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Meaning from "./Meaning"
+import Phonetics from "./Phonetics"
 
 export default function Definitions(props) {
 	if (props.definitions) {
@@ -10,6 +11,13 @@ export default function Definitions(props) {
 			<h2>
 				{props.definitions.word}
 			</h2>
+			{props.definitions.phonetics.map(function(phonetic, index) {
+				return (
+					<div key={index}>
+						<Phonetics phonetics={phonetic} />
+					</div>
+				)
+			})}
 			{props.definitions.meanings.map(function(meaning, index) {
 				return (
 					<div key={index}>
--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,7 @@ import React from "react"
 import Synonyms from "./Synonyms"
 
 export default function Meaning(props) {
-	console.log(props.meaning)
+	//console.log(props.meaning)
 	return (
 		<div className="Meaning p-3">
 			<h4 className="text-capitalize">{props.meaning.partOfSpeech}</h4>
