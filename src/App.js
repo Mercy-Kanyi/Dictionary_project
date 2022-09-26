49258f78d16f07ce70a2b575e6b778cec9b9c018
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-09-26 19:10:43 +0300

    added the photos component and api

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import dictionary from "./Images/dictionary.png"
 
 function App() {
   return (
-    <div className="App m-4 p-3">
+    <div className="App p-1">
       <div className="container">
         <header className="App-header">
           <img src={dictionary} className="dictionary-image img-fluid w-25" alt="dictionary"/>
         </header>
-        <Dictionary defaultWordSearch="sunrise"/>
+        <Dictionary defaultWordSearch="color"/>
         <footer className="App-footer">
           <small>
             Coded by{" "}
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Definitions from "./Definitions";
+import Photos from "./Photos"
 import "./Dictionary.css"
 
 export default function Dictionary(props) {
   let [wordSearched, setWordSearched] = useState(props.defaultWordSearch);
   let [definition, setDefinition] = useState(null);
   let [loaded, setLoaded] = useState(false)
+  let [photos, setPhotos] = useState(null)
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     //console.log(response.data[0]);
     setDefinition(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    //console.log(response)
+    setPhotos(response.data.photos)
+  }
+  
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordSearched}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000107f21550cabb472a9a924fefd54999fc";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${wordSearched}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` }; 
+    axios
+      .get(pexelsApiUrl, { headers: headers})
+      .then(handlePexelsResponse);
+    
   }
 
   function handleSearch(event) {
@@ -51,6 +67,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Definitions definitions={definition} />
+        <Photos photos={photos} />
       </div>
     );
 }
